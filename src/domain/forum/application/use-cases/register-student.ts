d3ff18fe6a98776546acb9682d4654293a88960a
Rogerio-17 <rogeriojmf10@gmail.com>
2024-11-12 22:15:08 -0300

    feat: finaliza casos de uso de cadastro e login

---
+++ src/domain/forum/application/use-cases/register-student.ts
@@ -0,0 +1,53 @@
+import { Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { Student } from '../../enterprise/entities/student'
+import { StudentRepository } from '../repositories/studants-repository'
+import { HashGenerator } from '../cryptography/hash-generator'
+import { StudentAlreadyExistsError } from './errors/student-already-exists-error'
+
+interface RegisterStudentUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+type RegisterStudentUseCaseResponse = Either<
+  StudentAlreadyExistsError,
+  {
+    student: Student
+  }
+>
+
+@Injectable()
+export class RegisterStudentUseCase {
+  constructor(
+    private studentRepository: StudentRepository,
+    private hashGenerator: HashGenerator,
+  ) {}
+
+  async execute({
+    email,
+    name,
+    password,
+  }: RegisterStudentUseCaseRequest): Promise<RegisterStudentUseCaseResponse> {
+    const studantWithSameEmail = await this.studentRepository.findByEmail(email)
+
+    if (studantWithSameEmail) {
+      return left(new StudentAlreadyExistsError(email))
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(password)
+
+    const student = Student.create({
+      email,
+      name,
+      password: hashedPassword,
+    })
+
+    await this.studentRepository.create(student)
+
+    return right({
+      student,
+    })
+  }
+}
