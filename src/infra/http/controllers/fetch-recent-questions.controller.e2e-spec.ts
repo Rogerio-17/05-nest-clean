ff28e57a1cc51f51d960c34755fdc1549b7415b2
Rogerio-17 <rogeriojmf10@gmail.com>
2024-12-02 18:48:42 -0300

    feat: refatora testes e cria factories do prisma que estavam faltando

--- src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -1,17 +1,14 @@
 import { AppModule } from '@/infra/app.module'
 import { DatabaseModule } from '@/infra/database/database.module'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
 import { QuestionFactory } from 'test/factories/make-question'
 import { StudentFactory } from 'test/factories/make-student'
-import { promise } from 'zod'
 
 describe('Fetch recent questions (E2E)', () => {
   let app: INestApplication
-  let prisma: PrismaService
   let studentFactory: StudentFactory
   let questionFactory: QuestionFactory
   let jwt: JwtService
@@ -23,8 +20,6 @@ describe('Fetch recent questions (E2E)', () => {
     }).compile()
 
     app = moduleRef.createNestApplication()
-
-    prisma = moduleRef.get(PrismaService)
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
     jwt = moduleRef.get(JwtService)
@@ -37,7 +32,7 @@ describe('Fetch recent questions (E2E)', () => {
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
-    Promise.all([
+    await Promise.all([
       questionFactory.makePrismaQuestion({ authorId: user.id, title: 'Title 1' }),
       questionFactory.makePrismaQuestion({ authorId: user.id, title: 'Title 2' })
     ])
@@ -50,8 +45,8 @@ describe('Fetch recent questions (E2E)', () => {
     expect(response.status).toBe(200)
     expect(response.body).toEqual({
       questions: [
-        expect.objectContaining({ title: 'Title 1' }),
         expect.objectContaining({ title: 'Title 2' }),
+        expect.objectContaining({ title: 'Title 1' }),
       ],
     })
   })
--- test/factories/make-question-comment.ts
+++ test/factories/make-question-comment.ts
@@ -4,6 +4,9 @@ import {
   QuestionComment,
   QuestionCommentProps,
 } from '@/domain/forum/enterprise/entities/question-comment'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
+import { PrismaQuestionCommentMapper } from '@/infra/database/prisma/mappers/prisma-question-comment-mapper'
 
 export function makeQuestionComment(
   overrides: Partial<QuestionCommentProps> = {},
@@ -20,4 +23,19 @@ export function makeQuestionComment(
   )
 
   return questionComment
+}
+
+@Injectable()
+export class QuestionCommentFactory {
+  constructor(private prisma: PrismaService) { }
+
+  async makePrismaQuestionComment(data: Partial<QuestionCommentProps> = {}): Promise<QuestionComment> {
+    const questioncomment = makeQuestionComment(data)
+
+    await this.prisma.comment.create({
+      data: PrismaQuestionCommentMapper.toPrisma(questioncomment)
+    })
+
+    return questioncomment
+  }
 }
